45e24e51358d0f1e801031fadfa58a492f1d09cc
besky <onlybesky@gmail.com>
2024-01-11 09:09:28 +0900

    edit: section 2, practice file of lab 1

--- src/main/java/org/example/section2/PatternedRectangularTilings.java
+++ src/main/java/org/example/section2/PatternedRectangularTilings.java
@@ -1,28 +1,19 @@
 package org.example.section2;
 
-import java.util.List;
-
 public class PatternedRectangularTilings {
-    private final static String[][] TILE_WHITE = {{"white"}, {"white", "white"}};
-    private final static String[][] TILE_RED = {{"red"}, {"red", "red"}};
-    private final static String[][] TILE_BLACK = {{"black"}};
-    private final static String[][] TILE_BLUE = {{"blue"}};
+    public static void main(String[] args) {
+        System.out.println(tiling(2));
+    }
 
-    public static List<List<String>> tiling(List<List<String>> tiles, int length){
+    public static int tiling(int length){
         if (length < 1){
-            return tiles;
+            return 1;
         }
         if (length == 1){
-            tiles.get(0).add(TILE_BLACK[0][0]);
-            tiles.get(1).add(TILE_BLUE[0][0]);
-            return tiles;
+            return 3; // black + blue / blue + black / blue + blue
         }
-        tiles.get(0).add(TILE_WHITE[0][0]);
-        for (String tile : TILE_WHITE[1]){
-            tiles.get(1).add(tile);
-        }
-        tiles.get(0).add(TILE_BLUE[0][0]);
-        return tiling(tiles, length - 2);
+        return 3 * tiling(length - 1) + 4 * tiling(length - 2);
+        // 4: red + black / red + blue / white + black / white + blue
     }
 
 }
